93c1ae5da0f9bf2ebf97033ee5faad8642e22983
AyushGupta7024 <ag0308958@gmail.com>
2023-01-11 08:43:41 +0530

    dartbasics

--- three.dart
+++ three.dart
@@ -1,57 +1,62 @@
 main() {
-  print("good morning");
-  
-  //var foodItem = "cake";
-  //String foodSource = "cake Factory";
-  //int foodItemCount = 4;
-  // double foodPrice = 50.5;
-  //dynamic customFoodField = "Cake plus";
-  //
   var myNumber = 9;
-  String myString = "hello";
-  String yourString = "cello";
+
+  String myString = "Hello";
+
+  String yourString = "Cello";
+
   String newString = myString + yourString;
+
   int myNewNum = 8;
+
   String newStringPro = "${myNewNum.isEven} $yourString";
+
   String foodPrice = "50";
-  String foodPriceDouble = "50.4";
+  String foodPriceDouble = "50.5";
+
   int convertedFoodPrice = int.parse(foodPrice);
+
   double convertedFoodPriceDouble = double.parse(foodPriceDouble);
-  bool myBool = (bool == "true") ? true : false;
-  print(myNewNum);
-  print(convertedFoodPrice);
-  print(newStringPro);
-  print(convertedFoodPriceDouble);
-  print(myBool);
-  //if (boolStr == "true"){
 
-  // }
-  //else if (boolStr == "false"){
+  String boolStr = "true";
 
-//}
-  int c = 0;
+  bool myBool = (boolStr == "true") ? true : false;
+
+  int myNewInt = 6;
+
+  String myNewIntStr = myNewInt.toString();
+
+  print(myNewIntStr.runtimeType);
+
+  if (boolStr == "true") {
+    // do something
+  } else if (boolStr == "false") {
+  } else {}
+
+  String c = "Hell1o";
+
   switch (c) {
-    case 1:
+    case "Hello":
       print(1.0);
       break;
-    case 2:
+    case "Yo":
       print(2.0);
       break;
-    default:
-      print("no matched");
-  }
-  String ans = "";
-  for (int i = 0; i < 10; i++) {
-    print(ans + " " + '${i}');
   }
 
-  
+  for (int i = 0; i < 10; i++) {
+    print(i);
+  }
+
+  int finalSum = giveRemainder(30, 3);
   int a = 9;
   print(a++);
   print(a++);
   print(++a);
 }
 
-int addNumbers(int a, int b) {
-  return a + b;
+int giveRemainder(int a, int b) {
+  return a % b;
 }
+
+
